2c6643d74e0250344db4d2dfc99edea28e16835d
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-06-08 20:31:35 +0330

    show all data ui in sidebar of

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import useFeach from "../../hooks/useFeach";
 
 function Hotels() {
@@ -11,11 +11,36 @@ function Hotels() {
     `q=${destination || ""}&accommodates_gte=${room || 1}`
   );
 
-  console.log(data);
-
   if (isLoading) <div>Loading...</div>;
 
-  return <div>{data.length}</div>;
+  return (
+    <div className="searchList">
+      <h2>Search Results ({data.length})</h2>
+      {data.map((item) => {
+        return (
+          <Link
+            key={item.id}
+            to={`/hotels/${item.id}?lat=${item.latitude}&long=${item.longitude}`}
+          >
+            <div className="searchItem">
+              <img
+                src={item.thumbnail_url}
+                alt={item.name}
+                style={{ border: "1px solid rgb(225, 225, 225)" }}
+              />
+              <div className="searchItemDesc">
+                <p className="location">{item.smart_location}</p>
+                <p className="name">{item.name}</p>
+                <p className="price">
+                  €&nbsp;{item.price}&nbsp;<span>night</span>
+                </p>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Hotels;
