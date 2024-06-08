588a0fbb16465b850cc3b5de93f482753f40c6f9
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-06-08 21:39:16 +0330

    import React Leaflet for map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,31 @@
-import { useHotels } from "../context/HotelsProvider"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useHotels } from "../context/HotelsProvider";
+import { useState } from "react";
 
 function Map() {
-    const {isLoading , hotels} = useHotels()
+  const { isLoading, hotels } = useHotels();
+  const [mapCenter, setMapCenter] = useState([52.366527, 4.868144]);
+
   return (
-    <div className="mapContainer">Map</div>
-  )
+    <MapContainer
+      className="map"
+      center={mapCenter}
+      zoom={13}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {hotels.map((item) => (
+        <Marker key={item.id} position={[item.latitude, item.longitude]}>
+          <Popup>
+           {item.host_location}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
 
-export default Map
+export default Map;
