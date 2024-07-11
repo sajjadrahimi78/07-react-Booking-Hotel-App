7ad6b5ca94560d1a00c60ca47e699d52af3aef43
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-07-11 12:55:40 +0330

    advanceed state management pattern in context and useReducer

--- src/components/context/BootmarkListContext.jsx
+++ src/components/context/BootmarkListContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import axios from "axios";
 import toast from "react-hot-toast/headless";
 
@@ -6,21 +12,68 @@ const BookmarkContext = createContext();
 
 const BASE_URL = "http://localhost:5000";
 
+const intialState = {
+  bookmarks: [],
+  isLoading: false,
+  currentBookmark: null,
+  error: null,
+};
+
+// در اینحا باید توابع ما به صورت پیور باشه ولی در این پروژه توابع ما چون حداقل دیتا رو فچ میکنه پس ساید افکت داره و نمیشه اینجا استفاده کرد
+function bookmarkReducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+    case "bookmarks/loaded":
+      return { ...state, bookmarks: action.payload, isLoading: false };
+    case "bookmark/loaded":
+      return { ...state, isLoading: false, currentBookmark: action.payload };
+    case "bookmark/created":
+      return {
+        ...state,
+        isLoading: false,
+        bookmarks: [...state.bookmarks, action.payload],
+        currentBookmark: action.payload,
+      };
+    case "bookmark/deleted":
+      return {
+        ...state,
+        isLoading: false,
+        bookmarks: state.bookmarks.filter((item) => item.id !== action.payload),
+        currentBookmark: null,
+      };
+    case "rejected":
+      return { ...state, isLoading: false, error: action.payload };
+
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+// we have a async action => 1.pending 2.success 3.rejected
+
 function BookmarkListProvider({ children }) {
-  const [currentBookmark, setCurrentBookmark] = useState(null);
-  const [bookmarks, setBookmarks] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [currentBookmark, setCurrentBookmark] = useState(null);
+  // const [bookmarks, setBookmarks] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const [{ bookmarks, isLoading, currentBookmark }, dispatch] = useReducer(
+    bookmarkReducer,
+    intialState
+  );
 
   useEffect(() => {
     async function fetchBookmarkList() {
-      setIsLoading(true);
+      dispatch({ type: "loading" });
       try {
         const { data } = await axios.get(`${BASE_URL}/bookmarks`);
-        setBookmarks(data);
+        dispatch({ type: "bookmarks/loaded", payload: data });
       } catch (error) {
         toast.error(error.message);
-      } finally {
-        setIsLoading(false);
+        dispatch({
+          type: "rejected",
+          payload: "an error occurred in loading bookmarks",
+        });
       }
     }
 
@@ -28,40 +81,41 @@ function BookmarkListProvider({ children }) {
   }, []);
 
   async function getBookmark(id) {
-    setIsLoading(true);
-    setCurrentBookmark(null);
+    if (+id === currentBookmark?.id) return;
+
+    dispatch({ type: "loading" });
     try {
       const { data } = await axios.get(`${BASE_URL}/bookmarks/${id}`);
-      setCurrentBookmark(data);
-      setIsLoading(false);
+      dispatch({ type: "bookmark/loaded", payload: data });
     } catch (error) {
       toast.error(error.message);
-      setIsLoading(false);
+      dispatch({
+        type: "rejected",
+        payload: "an error occurred in fetching single bookmark",
+      });
     }
   }
 
   async function createBookmark(newBookmark) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       const { data } = await axios.post(`${BASE_URL}/bookmarks/`, newBookmark);
-      setCurrentBookmark(data);
-      setBookmarks((prev) => [...prev, data]);
+      dispatch({ type: "bookmark/created", payload: data });
     } catch (error) {
       toast.error(error.message);
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
 
   async function deleteBookmark(id) {
-    setIsLoading(true);
+    dispatch({ type: "loading" });
     try {
       await axios.delete(`${BASE_URL}/bookmarks/${id}`);
-      setBookmarks((prev) => prev.filter(item => item.id !== id));
+      dispatch({ type: "bookmark/deleted", payload: id });
+      // setBookmarks((prev) => prev.filter((item) => item.id !== id));
     } catch (error) {
       toast.error(error.message);
-    } finally {
-      setIsLoading(false);
+      dispatch({ type: "rejected", payload: error.message });
     }
   }
 
@@ -73,7 +127,7 @@ function BookmarkListProvider({ children }) {
         bookmarks,
         getBookmark,
         createBookmark,
-        deleteBookmark
+        deleteBookmark,
       }}
     >
       {children}
@@ -86,3 +140,7 @@ export default BookmarkListProvider;
 export function useBookmark() {
   return useContext(BookmarkContext);
 }
+
+// context + reducer => value ={{state , dispatch}} => SYNC ACTION => (no sync action)
+
+// ACYNC ACTION => reducer function is a PURE function!!
